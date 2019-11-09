d8de9a66f1ccfac90decd43a3b66cd03d003ded0
Daniel Cunha <danielcunha54@gmail.com>
2019-11-09 19:56:16 -0300

    Add vscode extension and git alias

--- config/linux/post_installs.sh
+++ config/linux/post_installs.sh
@@ -14,6 +14,8 @@ setup_git() {
     git config --global alias.st status
     git config --global alias.br branch
     git config --global alias.ck checkout
+    git config --global alias.cm commit
+    git config --global alias.ad 'add .'
     git config --global alias.unstage 'reset HEAD --'
     git config --global alias.last 'log -1 HEAD'
 }
